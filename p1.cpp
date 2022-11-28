d31208d340cb0c9b61659463b3f0b5340dba9fc7
Shad0wAVM <arturvasco.martins@gmail.com>
2022-11-28 10:03:40 +0000

    List of all possible squares (list of vectors; each vector has 3 values: n position, m position and the side lenght)

--- p1.cpp
+++ p1.cpp
@@ -6,18 +6,27 @@ using namespace std;
 
 unsigned int _N, _M;
 vector<int> _path;
+list<vector<int>>* _possibleSquares;
 
-void readGraph() {
+void readGraph()
+{
   cin >> _N >> _M;
   _path.resize(_N);
-  for (size_t i = 0; i < _N; i++) {
+  for (size_t i = 0; i < _N; i++)
+  {
     int a;
     cin >> a;
     _path[i] = a;
   }
 }
 
-int main() {
+void findAllPossibleSquares()
+{
+}
+
+int main()
+{
   readGraph();
+  findAllPossibleSquares();
   return 0;
-}  
+}
