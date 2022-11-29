532c7200e6ca4f89e53be918e1626cdbc9d4ba6b
Shad0wAVM <arturvasco.martins@gmail.com>
2022-11-29 09:41:16 +0000

    Inicio do findAllPossibleSquares

--- p1.cpp
+++ p1.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <array>
 
 using namespace std;
 
 unsigned int _N, _M;
 vector<int> _path;
-list<vector<int>>* _possibleSquares;
+vector<array<int,3>> _possibleSquares;
 
 void readGraph()
 {
   cin >> _N >> _M;
-  _path.resize(_N);
   for (size_t i = 0; i < _N; i++)
   {
     int a;
     cin >> a;
-    _path[i] = a;
+    _path.push_back(a);
   }
 }
 
 void findAllPossibleSquares()
 {
+  for (unsigned int a = 0; a < _N; a++) {
+    int height = _N - a;
+    for (int i = 0; i < _path[a]; i++) {
+      for (int b = height; b > 1; b--) {
+
+      }
+    }
+  }
 }
 
 int main()
