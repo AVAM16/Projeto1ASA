b0da0da54069bc4b64b6d172d5a2103150c31c6c
Shad0wAVM <arturvasco.martins@gmail.com>
2022-12-02 20:47:29 +0000

    Projeto aparentemente a funcionar

--- p1.cpp
+++ p1.cpp
@@ -49,7 +49,7 @@ int squareOverlap(vector<int> Square1, vector <int> Square2)
   int Size2 = Square2.at(2);
   for (int i = n1; i < n1 + Size1; i++) {
     for (int j = m1; j < m1 + Size1; j++) {
-      if ((i <= n2 + Size2 && i >= n2 && j < m2 + Size2 && j > m2)) {
+      if (((i < n2 + Size2 && i >= n2) && (j < m2 + Size2 && j >= m2))) {
         return 1;
       }
     }
@@ -69,6 +69,8 @@ int countOptionsRecursive(vector<vector<int>> auxiliarvector, vector<int> Square
   for(vector<int> Square1: _possibleSquares){
     if(j <= k){
       for(vector<int> Square2: _auxiliarvector2){
+        cout << Square1.at(0) << Square1.at(1) << Square1.at(2) << endl;
+        cout << Square2.at(0) << Square2.at(1) << Square2.at(2) << endl;
         if(squareOverlap(Square2,Square1) == 1){
           l = false;
           cout << "errou" << endl;
@@ -95,6 +97,7 @@ int countOptions(){
   int j = 0;
   vector<vector<int>> _auxiliarvector;
   for(vector<int> Square1: _possibleSquares){
+    _auxiliarvector.clear();
     _auxiliarvector.push_back(Square1);
     j++;
     cout << j << endl;
@@ -110,11 +113,10 @@ int main()
 {
   readGraph();
   findAllPossibleSquares();
-  /* vector<int> square = _possibleSquares.at(7);
-  cout << square.at(0) << endl;
-  cout << square.at(1) << endl;
-  cout << square.at(2) << endl; */
   int i = countOptions();
   cout << i << endl;
+  //vector<int> v1 = _possibleSquares.at(1);
+  //vector<int> v2 = _possibleSquares.at(7);
+  //cout << squareOverlap(v2, v1) << endl;
   return 0;
 }
